b749284e1bf26089c9f943afe9c529bd5712005d
whiteov3rflow <djibrilow@gmail.com>
2025-05-22 10:34:43 +0200

    New version of the npc attack

--- libs/npc.py
+++ libs/npc.py
@@ -1,10 +1,11 @@
-# import player
 import random
+
 
 class Npc():
     
     def __init__(self):
         self.hp = 100
+        self.actions = ['attack', 'defend','heal'] 
         self.attacks = {"Basic":10,"Powerfull":50,"Fatal":100}    
 
     def get_hp(self) -> int:
@@ -12,11 +13,30 @@ class Npc():
         #print("[+] Getting the HP of the NPC")
         return self.hp
     
+    def set_hp(self,hp):
+        """Setter for the npc hp"""
+        self.hp += hp
+        return self.hp
+    
     def get_attacks(self) -> int:
         """
         This function get a random attacks from the dict
         """
-        return random.choice(list(self.attacks.values()))
- 
+        self.attack = random.choice(list(self.attacks.values()))
+        return self.attack
+    
+    # def get_actions(self) -> str:
+    #     return random.choice(self.actions) #return a random actions betwen "attack,defend,heal"
 
-# npc = Npc()
+    # def turn(self):
+
+    #     hp = self.get_hp()
+    #     if hp < 50:
+    #         self.set_hp(10)
+    #         return self.set_hp(10)
+        
+    #     elif hp >= 50 and hp < 80:
+    #         return self.get_attacks()
+        
+        
+npc = Npc()
